1f4b9de2c2e27b411c084c297517507e6672186b
Gerhard Pazout <gerhard.pazout@gmx.at>
2025-06-17 19:25:52 +0200

    [TASK] Add basic sending and receiving of data

--- server_client_communication/client.py
+++ server_client_communication/client.py
@@ -1,14 +1,28 @@
 import socket
 import time
 
+# Server IP and port
 SERVER_IP = "192.168.1.100"  # Use your actual server IP if remote
 PORT = 3333
+
+# Markers and payload
+START = "SENDING_DATA\n"
+END = "DATA_SENT"
+DATA = "1.0,2.0,3.0,4.0,5.0,6.0"
 
 # Create socket and connect
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((SERVER_IP, PORT))
 
 print("Connected to server.")
+
+print("Sending data...")
+sock.sendall(START.encode('utf-8'))
+time.sleep(0.05)
+sock.sendall(DATA.encode('utf-8'))
+time.sleep(0.05)
+sock.sendall(END.encode('utf-8'))
+print("Data sent")
 
 input("Press Enter to disconnect...")
 sock.close()
--- server_client_communication/server.py
+++ server_client_communication/server.py
@@ -8,6 +8,9 @@ import socket
 HOST = socket.gethostbyname(socket.gethostname())  # or "0.0.0.0"
 PORT = 3333
 
+START_MARKER = "SENDING_DATA"
+END_MARKER = "DATA_SENT"
+
 # Create TCP socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
@@ -19,6 +22,37 @@ print("Waiting for a client to connect...")
 # Wait and accept a client
 conn, addr = server.accept()
 print(f"Client connected from {addr[0]}:{addr[1]}")
+
+buffer = ""
+msg = ""
+receiving = False
+
+while True:
+    data = conn.recv(1024)
+    if not data:
+        print("[-] Client disconnected unexpectedly")
+        break
+
+    decoded = data.decode('utf-8')
+    buffer += decoded
+
+    if not receiving:
+        if START_MARKER in buffer:
+            receiving = True
+            buffer = buffer.split(START_MARKER, 1)[1]  # remove marker
+        else:
+            continue
+
+    if END_MARKER in buffer:
+        msg += buffer.split(END_MARKER, 1)[0]
+        break
+    else:
+        msg += buffer
+        buffer = ""
+
+# --- Print the received message ---
+print("Received data:")
+print(msg)
 
 # Just keep the connection open
 input("Press Enter to close connection...")
